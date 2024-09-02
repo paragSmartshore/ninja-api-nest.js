5303268b5522da51da8c0d68689b660d7447e3dd
paragdangui <33660969+paragdangui@users.noreply.github.com>
2024-09-02 11:49:27 +0530

    Added Swagger Documentation

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,9 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEnum, MinLength } from 'class-validator';
 
 export class CreateNinjaDto {
+  @ApiProperty({
+    description: 'The name of the ninja',
+    minLength: 3,
+    example: 'Naruto',
+  })
   @MinLength(3)
   name: string;
 
+  @ApiProperty({
+    description: 'The weapon of the ninja',
+    enum: ['stars', 'nunchucks'],
+    example: 'stars',
+  })
   @IsEnum(['stars', 'nunchucks'], { message: 'Use the correct Weapon!' })
   weapon: 'stars' | 'nunchucks';
 }
--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -16,18 +16,37 @@ import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
 import { BeltGuard } from 'src/belt/belt.guard';
+import {
+  ApiCreatedResponse,
+  ApiParam,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Ninja } from './entities/ninja.entity';
 
+@ApiTags('Ninjas')
 @Controller('ninjas')
 @UseGuards(BeltGuard)
 export class NinjasController {
   constructor(private readonly ninjaService: NinjasService) {}
   //GET /ninjas?weapon=stars--> []
+  @ApiQuery({
+    name: 'weapon',
+    required: false,
+    enum: ['stars', 'nunchucks'],
+    description: 'Filter ninjas by weapon type',
+  })
   @Get()
   getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks') {
     // const service = new NinjasService();
     return this.ninjaService.getNinjas(weapon);
   }
   //GET /ninjas/:id	 --> {...}
+  @ApiParam({
+    name: 'id',
+    description: 'The ID of the ninja to retrieve',
+    example: 1,
+  })
   @Get(':id')
   getOneNinja(@Param('id', ParseIntPipe) id: number) {
     try {
@@ -38,6 +57,7 @@ export class NinjasController {
     }
   }
   //POST /ninjas
+  @ApiCreatedResponse({ type: Ninja })
   @Post()
   createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjaService.createNinja(createNinjaDto);
